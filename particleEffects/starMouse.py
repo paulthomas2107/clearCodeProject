00b656e90ba9d565b6943e838ce2a0fef834d6a2
Paul Thomas <paulthomas2107@gmail.com>
2022-11-30 15:47:43 +0000

    More.,.....

---
+++ particleEffects/starMouse.py
@@ -0,0 +1,59 @@
+import sys
+import pygame
+import random
+
+
+class ParticleStar:
+    def __init__(self):
+        self.particles = []
+        self.surface = pygame.image.load('star.png').convert_alpha()
+        self.width = self.surface.get_rect().width
+        self.height = self.surface.get_rect().height
+
+    def emit(self):
+        if self.particles:
+            self.delete_particles()
+            for particle in self.particles:
+                particle[0].x += particle[1]
+                particle[0].y += particle[2]
+                particle[3] -= 0.2
+                screen.blit(self.surface, particle[0])
+
+    def add_particles(self):
+        pos_x = pygame.mouse.get_pos()[0] - self.width / 2
+        pos_y = pygame.mouse.get_pos()[1] - self.width / 2
+        direction_x = random.randint(-3, 3)
+        direction_y = random.randint(-3, 3)
+        lifetime = random.randint(4, 10)
+        particle_rect = pygame.Rect(pos_x, pos_y, self.width, self.height)
+        self.particles.append([particle_rect, direction_x, direction_y, lifetime])
+
+    def delete_particles(self):
+        particle_copy = [particle for particle in self.particles if particle[3] > 0]
+        self.particles = particle_copy
+
+
+pygame.init()
+pygame.display.set_caption("Particle Effects (Nyan)")
+screen = pygame.display.set_mode((1000, 800))
+clock = pygame.time.Clock()
+
+particle = ParticleStar()
+
+nyan_surface = pygame.image.load('nyan_cat.png').convert_alpha()
+
+PARTICLE_EVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(PARTICLE_EVENT, 40)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == PARTICLE_EVENT:
+            particle.add_particles()
+
+    screen.fill((30, 30, 30))
+    particle.emit()
+    pygame.display.update()
+    clock.tick(120)
