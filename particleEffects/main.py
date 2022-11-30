6528a950e227519f64d7068a3084650aad8a2e89
Paul Thomas <paulthomas2107@gmail.com>
2022-11-30 14:19:42 +0000

    Initial

--- particleEffects/main.py
+++ particleEffects/main.py
@@ -2,17 +2,54 @@ import sys
 
 import pygame
 
+
+class ParticlePrinciple:
+    def __init__(self):
+        self.particles = []
+
+    def emit(self):
+        if self.particles:
+            self.delete_particles()
+            for particle in self.particles:
+                # move, shrink and draw circle
+                particle[0][1] += particle[2]
+                particle[1] -= 0.2
+                pygame.draw.circle(screen, pygame.Color('White'), particle[0], int(particle[1]))
+
+    def add_particles(self):
+        pos_x = 500
+        pos_y = 400
+        radius = 10
+        direction = -1
+        particle_circle = [[pos_x, pos_y], radius, direction]
+        self.particles.append(particle_circle)
+
+    def delete_particles(self):
+        particle_copy = [particle for particle in self.particles if particle[1] > 0]
+        self.particles = particle_copy
+
+
 pygame.init()
 pygame.display.set_caption("Particle Effects")
 screen = pygame.display.set_mode((1000, 800))
 clock = pygame.time.Clock()
+
+particle1 = ParticlePrinciple()
+
+PARTICLE_EVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(PARTICLE_EVENT, 40)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == PARTICLE_EVENT:
+            particle1.add_particles()
 
     screen.fill((30, 30, 30))
+
+    particle1.emit()
+
     pygame.display.update()
     clock.tick(120)
